9b789b885937e9a7f44a6ca784736b9a83f02d2b
tajaea <55627073+tajaea@users.noreply.github.com>
2021-04-19 15:52:57 -0500

    made changes

--- AP Group Project/src/com/microstar/cablevision/views/ViewPaymentStatus.java
+++ AP Group Project/src/com/microstar/cablevision/views/ViewPaymentStatus.java
@@ -18,6 +18,7 @@ import microStarCableVision.Customer;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
 
 public class ViewPaymentStatus {
 
@@ -28,6 +29,7 @@ public class ViewPaymentStatus {
 	private JLabel customerIDValue;
 	private JLabel lastNameValue  ;
 	private JLabel paymentStatus ;
+	private JLabel logoImageLabel;
 	/**
 	 * Launch the application.
 	 */
@@ -59,45 +61,49 @@ public class ViewPaymentStatus {
 	private void initialize(Client con) {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(new Color(255, 245, 238));
-		frame.getContentPane().setMinimumSize(new Dimension(800, 500));
-		frame.getContentPane().setMaximumSize(new Dimension(800, 500));
-		frame.setMinimumSize(new Dimension(800, 500));
-		frame.setMaximumSize(new Dimension(800, 500));
-		frame.setResizable(false);
+		frame.getContentPane().setMinimumSize(new Dimension(750, 350));
+		frame.getContentPane().setMaximumSize(new Dimension(750, 350));
+		frame.setMinimumSize(new Dimension(750, 350));
+		frame.setMaximumSize(new Dimension(750, 350));
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		ViewPaymentStatuscontrol = new ViewPaymentStatusController(this);
 		ViewPaymentStatuscontrol.setClient(con);
+		
+		logoImageLabel = new JLabel("");
+		logoImageLabel.setIcon(new ImageIcon("images\\star.png"));
+		logoImageLabel.setBounds(10, 11, 110, 100);
+		frame.getContentPane().add(logoImageLabel);
 		JLabel customerIDLabel = new JLabel("Customer ID");
 		customerIDLabel.setFont(new Font("Dubai", Font.PLAIN, 20));
-		customerIDLabel.setBounds(10, 11, 116, 35);
+		customerIDLabel.setBounds(149, 11, 116, 35);
 		frame.getContentPane().add(customerIDLabel);
 		
 		 customerIDValue = new JLabel("value");
 		customerIDValue.setFont(new Font("Dubai", Font.PLAIN, 17));
-		customerIDValue.setBounds(136, 11, 101, 35);
+		customerIDValue.setBounds(275, 11, 101, 35);
 		frame.getContentPane().add(customerIDValue);
 		
 		JLabel lastNameLabel = new JLabel("Last Name");
 		lastNameLabel.setFont(new Font("Dubai", Font.PLAIN, 20));
-		lastNameLabel.setBounds(10, 57, 116, 35);
+		lastNameLabel.setBounds(149, 57, 116, 35);
 		frame.getContentPane().add(lastNameLabel);
 		
 		lastNameValue = new JLabel("value");
 		lastNameValue.setFont(new Font("Dubai", Font.PLAIN, 20));
-		lastNameValue.setBounds(136, 57, 80, 35);
+		lastNameValue.setBounds(275, 57, 80, 35);
 		frame.getContentPane().add(lastNameValue);
 		
 		paymentStatus = new JLabel("Your Account Has An Oustanding Balance");
 		paymentStatus.setFont(new Font("Dubai", Font.PLAIN, 25));
-		paymentStatus.setBounds(10, 104, 560, 154);
+		paymentStatus.setBounds(149, 103, 560, 112);
 		frame.getContentPane().add(paymentStatus);
 		
 		 exitButton = new JButton("Exit");
 		
-		exitButton.setBounds(10, 425, 116, 35);
+		exitButton.setBounds(10, 265, 116, 35);
 		frame.getContentPane().add(exitButton);
 		frame.setVisible(true);
 		RegisterListener();
