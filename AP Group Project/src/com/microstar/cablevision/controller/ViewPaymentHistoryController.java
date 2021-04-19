3a8d0b6fa5cd2c1b925ca7a8134303ba6f09a436
Ryanrichie <56200781+RyanWhyte2000@users.noreply.github.com>
2021-04-19 15:05:22 -0400

    changes

--- AP Group Project/src/com/microstar/cablevision/controller/ViewPaymentHistoryController.java
+++ AP Group Project/src/com/microstar/cablevision/controller/ViewPaymentHistoryController.java
@@ -1,5 +1,74 @@
 package com.microstar.cablevision.controller;
+import microStarCableVision.Client;
+import microStarCableVision.Customer;
+import microStarCableVision.Complaint;
+
+import java.util.ArrayList;
+
+import com.microstar.cablevision.views.CustomerDashBoard;
+import com.microstar.cablevision.views.ViewPaymentHistory;
+
 
 public class ViewPaymentHistoryController {
 
+	ViewPaymentHistory ViewPaymentHistory;
+	
+	Complaint complaintObj; 
+	Customer customerObj;
+	private Client con;
+	ArrayList<Complaint> arrayList;
+	public ViewPaymentHistoryController( ViewPaymentHistory  ViewPaymentHistory) {
+		this. ViewPaymentHistory =  ViewPaymentHistory;
+		
+		
+	}
+	
+	public void setClient(Client client) {
+		con = client;//Assigning the Client instance that was created
+	}
+	
+	public void returnCustomer() {
+		con.sendAction("Get Complaint");
+		System.out.println(customerObj);
+		con.sendCustomer(customerObj);
+	}
+	
+	public void returnToCustomerDashboard() {
+		//viewPaymentHistory.getFrame().dispose();//close  a  window
+		new CustomerDashBoard(con).setCustomerObject(customerObj);// return to customer dashboard
+	}
+	
+	
+	
+	public void setCustomerInformation(Customer customerObj) {
+
+		con.sendAction("");
+		
+		this.customerObj=customerObj;//Assign the customer object from the database to the object that is in the controller
+		
+		ViewPaymentHistory.getCustomerIDValue().setText(this.customerObj.getCustomerID());
+		ViewPaymentHistory.getLastNameValue().setText(this.customerObj.getLastName());
+		//ViewPaymentHistory.getPaymentStatus().setText("");
+		 		
+			//arrayList = new ArrayList<Complaint>();
+		
+		arrayList=con.readCustomerComplaint(); // Returns an arraylist of the complaints made by the signed in ucustomer
+		
+		for(Complaint complain:arrayList) {
+			
+			ViewPaymentHistory.getModel().insertRow(ViewPaymentHistory.getTable().getModel().getRowCount(), new Object[] {
+				
+					
+			complain.getComplaintID(),complain.getCustomerId(),complain.getType(),
+				complain.getDetails(),complain.getStatus(),complain.getComplaintDate(),
+				complain.getComplaintTime()
+			});
+		}
+
+	} 
+	public void viewTableDetails() {
+		int index = ViewPaymentHistory.getTable().getSelectedRow();
+		System.out.println(arrayList.get(index));
+	}
+	
 }
--- AP Group Project/src/com/microstar/cablevision/views/ViewPaymentHistory.java
+++ AP Group Project/src/com/microstar/cablevision/views/ViewPaymentHistory.java
@@ -6,21 +6,39 @@ import javax.swing.JFrame;
 import java.awt.Dimension;
 import javax.swing.JLabel;
 
+import com.microstar.cablevision.controller.ViewPaymentHistoryController;
+import com.microstar.cablevision.controller.ViewPaymentStatusController;
 import com.microstar.cablevision.security.Security;
 
+import microStarCableVision.Client;
+import microStarCableVision.Customer;
+
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.Color;
 import javax.swing.JButton;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
 
 public class ViewPaymentHistory {
 
 	private JFrame frame;
+	private JTable table;
+	private JLabel customerIDValue;
+	private JLabel lastNameValue ;
+	private JButton exitButton;
+	DefaultTableModel Model = new DefaultTableModel();
 
+	ViewPaymentHistoryController ViewPaymentHistorycontrol;
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
+		/*EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					ViewPaymentHistory window = new ViewPaymentHistory();
@@ -30,20 +48,22 @@ public class ViewPaymentHistory {
 					Security.logger.error("An exception was caught in the main method of the ViewPaymentHistory class");
 				}
 			}
-		});
+		});*/
+		
+		new ViewPaymentHistory(new Client ());
 	}
 
 	/**
 	 * Create the application.
 	 */
-	public ViewPaymentHistory() {
-		initialize();
+	public ViewPaymentHistory(Client con) {
+		initialize(con);
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
+	private void initialize(Client con) {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(new Color(255, 245, 238));
 		frame.getContentPane().setMinimumSize(new Dimension(800, 500));
@@ -55,14 +75,17 @@ public class ViewPaymentHistory {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
+		ViewPaymentHistorycontrol = new ViewPaymentHistoryController(this);
+		ViewPaymentHistorycontrol.setClient(con);
+		
 		JLabel customerIDLabel = new JLabel("Customer ID");
 		customerIDLabel.setFont(new Font("Dubai", Font.PLAIN, 20));
 		customerIDLabel.setBounds(10, 11, 116, 35);
 		frame.getContentPane().add(customerIDLabel);
 		
-		JLabel customerIDValue = new JLabel("CUS0652e6");
+		 customerIDValue = new JLabel("CUS0652e6");
 		customerIDValue.setFont(new Font("Dubai", Font.PLAIN, 17));
-		customerIDValue.setBounds(136, 11, 101, 35);
+		customerIDValue.setBounds(136, 11, 164, 35);
 		frame.getContentPane().add(customerIDValue);
 		
 		JLabel lastNameLabel = new JLabel("Last Name");
@@ -70,19 +93,114 @@ public class ViewPaymentHistory {
 		lastNameLabel.setBounds(10, 57, 116, 35);
 		frame.getContentPane().add(lastNameLabel);
 		
-		JLabel lastNameValue = new JLabel("Anglin");
+		 lastNameValue = new JLabel("Anglin");
 		lastNameValue.setFont(new Font("Dubai", Font.PLAIN, 20));
-		lastNameValue.setBounds(136, 57, 80, 35);
+		lastNameValue.setBounds(136, 57, 152, 35);
 		frame.getContentPane().add(lastNameValue);
 		
-		JLabel paymentStatus = new JLabel("Your Account is Up To Date");
-		paymentStatus.setFont(new Font("Dubai", Font.PLAIN, 25));
-		paymentStatus.setBounds(10, 104, 560, 154);
-		frame.getContentPane().add(paymentStatus);
-		
-		JButton exitButton = new JButton("Exit");
+		 exitButton = new JButton("Exit");
 		exitButton.setBounds(10, 425, 116, 35);
 		frame.getContentPane().add(exitButton);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 116, 774, 298);
+		frame.getContentPane().add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent event) {
+				if(event.getClickCount() == 2) {
+					ViewPaymentHistorycontrol.viewTableDetails();
+				}
+			}
+		});
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Payment ID", "Customer ID", " Amount Paid", " Balance Remaining", " Date Paid", " Payment Method"
+			}
+		) {
+			boolean[] columnEditables = new boolean[] {
+				false, false, false, false, false, false
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		table.getColumnModel().getColumn(0).setResizable(false);
+		table.getColumnModel().getColumn(1).setResizable(false);
+		table.getColumnModel().getColumn(2).setResizable(false);
+		table.getColumnModel().getColumn(2).setPreferredWidth(100);
+		table.getColumnModel().getColumn(3).setResizable(false);
+		table.getColumnModel().getColumn(4).setResizable(false);
+		table.getColumnModel().getColumn(5).setResizable(false);
+		table.getColumnModel().getColumn(5).setPreferredWidth(97);
+		
+
+		Model = (DefaultTableModel) table.getModel();
 		frame.setVisible(true);
+		RegisterListener();
 	}
+		public void RegisterListener() {
+		
+		exitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+				//ViewPaymentStatuscontrol.returnToCustomerDashboard();
+			}
+		});
+	}
+		public void setCustomerInformation(Customer customerObj) {
+
+			//this.customerObj=customerObj;
+			ViewPaymentHistorycontrol.setCustomerInformation(customerObj);
+		}
+
+		public JFrame getFrame() {
+			return frame;
+		}
+
+		public void setFrame(JFrame frame) {
+			this.frame = frame;
+		}
+
+		public JTable getTable() {
+			return table;
+		}
+
+		public void setTable(JTable table) {
+			this.table = table;
+		}
+
+		public JLabel getCustomerIDValue() {
+			return customerIDValue;
+		}
+
+		public void setCustomerIDValue(JLabel customerIDValue) {
+			this.customerIDValue = customerIDValue;
+		}
+
+		public JLabel getLastNameValue() {
+			return lastNameValue;
+		}
+
+		public void setLastNameValue(JLabel lastNameValue) {
+			this.lastNameValue = lastNameValue;
+		}
+
+		
+
+		public DefaultTableModel getModel() {
+			return Model;
+		}
+
+		public void setModel(DefaultTableModel model) {
+			Model = model;
+		}
+		
+		
+		
 }
